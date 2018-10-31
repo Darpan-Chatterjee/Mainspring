7273c958e15a6cd9b8ea4a232d24672d61c680d0
Darpan Chatterjee <darpan.chatterjee@gmail.com>
2018-10-31 19:48:08 +0530

    Day end Push

--- src/test/java/com/Mainspring/Tests/MainspringReleaseCreation.java
+++ src/test/java/com/Mainspring/Tests/MainspringReleaseCreation.java
@@ -1,5 +1,8 @@
 package com.Mainspring.Tests;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 import com.Mainspring.core.BaseTest;
@@ -9,6 +12,7 @@ public class MainspringReleaseCreation extends BaseTest {
 	@Test
 	public void AddRelease()
 	{
+		ArrayList<HashMap<String,String>> dataList =excelUtil.GetExcelDataExtract(masterExcel, "Release");
 		driver.get("https://mainspring.cognizant.com");
 	}
 
--- src/test/java/com/Mainspring/core/BaseTest.java
+++ src/test/java/com/Mainspring/core/BaseTest.java
@@ -3,20 +3,26 @@ package com.Mainspring.core;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import com.Mainspring.Utilities.DriverSet;
+import com.Mainspring.Utilities.ExcelUtility;
 import com.Mainspring.Utilities.PropertyFileData;
 
 public class BaseTest {
 	
 	public WebDriver driver=null;
 	public Properties prop = null;
+	public XSSFWorkbook masterExcel=null;
+	public ExcelUtility excelUtil=null;
+	
 	public String browserPath;
 	public int timeOutLimit;
-	
+	public String masterExcelPath=null;
+	public String masterExcelName=null;
 	
 	public PropertyFileData dataObj= new PropertyFileData();
 	
@@ -26,8 +32,14 @@ public class BaseTest {
 		prop = dataObj.getPropertyData("AutomationConfig");
 		browserPath=prop.getProperty("ChromePath");
 		timeOutLimit=Integer.parseInt(prop.getProperty("AutomationTimeout"));
+		masterExcelPath=prop.getProperty("MasterExcelPath");
+		masterExcelName=prop.getProperty("MasterExcelName");
+		
 		DriverSet getDriver= new DriverSet(browserPath,timeOutLimit);
+		excelUtil= new ExcelUtility(masterExcelPath,masterExcelName);
+		
 		driver= getDriver.openBrowser();
+		masterExcel=excelUtil.setExcel();
 	}
 	
 	@AfterTest
